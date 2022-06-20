45cd3e15f6e0108c9bf180eb0c6b63f9905ab552
Мухутдинов Искандер <iskanderka125@gmail.com>
2022-06-20 07:57:43 +0400

    Добавляет роутинг

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
 <!--  <img alt="Vue logo" src="./assets/logo.png">-->
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <p>
+    <router-link to="/">Home</router-link>
+    <router-link to="/blogs">Blogs</router-link>
+  </p>
+  <router-view></router-view>
+
 </template>
 
 <script>
