5c9f33bb3203d126e21e519830eb9b2baadefe2f
Мухутдинов Искандер <iskanderka125@gmail.com>
2022-06-21 01:37:19 +0400

    Добавляет основные иконки

--- src/components/Icon.vue
+++ src/components/Icon.vue
@@ -1,11 +1,63 @@
 <template>
-  <div>
-    Иконка
-  </div>
+  <svg
+      xmlns="http://www.w3.org/2000/svg"
+       :width="width"
+       :height="height"
+       viewBox="0 0 18 18"
+       :aria-labelledby="iconName"
+       role="img"
+  >
+    <title :id="iconName">{{ iconName }}</title>
+    <g :fill="iconColor">
+      <slot>
+        <component :is="iconCode"/>
+      </slot>
+    </g>
+  </svg>
 </template>
 
 <script>
-  export default {
+  import Arrow from './icons/Arrow';
+  import DoubleArrow from './icons/DoubleArrow';
+  import OutlineArrow from './icons/OutlineArrow';
+  import Magnifier from './icons/Magnifier';
 
+  import GithubLogo from './icons/GithubLogo';
+  import TelegramLogo from './icons/TelegramLogo';
+  import TwitterLogo from './icons/TwitterLogo';
+
+  export default {
+    components: {
+      Arrow,
+      DoubleArrow,
+      OutlineArrow,
+      Magnifier,
+      GithubLogo,
+      TelegramLogo,
+      TwitterLogo
+    },
+
+    props: {
+      iconName: {
+        type: String,
+        default: ''
+      },
+      iconCode: {
+        type: String,
+        default: '',
+      },
+      width: {
+        type: [Number, String],
+        default: 18
+      },
+      height: {
+        type: [Number, String],
+        default: 18
+      },
+      iconColor: {
+        type: String,
+        default: 'black'
+      }
+    },
   }
 </script>
--- src/root/_components/Header.vue
+++ src/root/_components/Header.vue
@@ -3,15 +3,18 @@
     <Button title="ТестКнопка" mode="primary"/>
     <Button title="ТестКнопка2" mode="secondary"/>
     <Button title="ТестКнопка3" mode="tertiary"/>
+    <Icon iconCode="Arrow"></Icon>
   </div>
 </template>
 
 <script>
   import Button from '@/components/Button';
+  import Icon from '@/components/Icon';
 
   export default {
     components: {
       Button,
+      Icon,
     }
   }
 </script>
