32505cc9a083ca91e85ddc4fd942853be6d39e6d
Мухутдинов Искандер <iskanderka125@gmail.com>
2022-06-22 03:07:10 +0400

    Добавляет контент для футера

---
+++ src/components/layout/footer/BrandInfo.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="wrapper">
+    <InlineSvg :src="require('@/assets/icons/tech-rate-logo.svg')" width="83" height="32" fill="white" title="TechRate"/>
+
+    <span class="text">@ 2022 by TechRate. All Rights Reserved</span>
+  </div>
+</template>
+
+<script>
+  import { defineComponent } from 'vue';
+
+  export default defineComponent({
+
+  })
+</script>
+
+<style lang="scss" scoped>
+  .wrapper {
+    display: flex;
+    flex-direction: column;
+    gap: 30px;
+  }
+
+  .text {
+    font-family: $font-base;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 12px;
+    line-height: 16px;
+    color: $text-color-gray;
+  }
+</style>
---
+++ src/components/layout/footer/Social.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="wrapper">
+    <template v-for="{logo, title} in buttons" :key="logo">
+      <a href="">
+        <InlineSvg class="" :src="require('@/assets/icons/' + logo +'.svg')" width="20" height="20" :title="title"/>
+      </a>
+    </template>
+  </div>
+</template>
+
+<script>
+  import { defineComponent } from 'vue';
+
+  export default defineComponent ({
+      setup() {
+        const buttons = [
+          {logo: 'telegram-logo', title: 'Телеграм',},
+          {logo: 'twitter-logo', title: 'Твитер',},
+          {logo: 'github-logo', title: 'Гитхаб',},
+        ]
+
+        return { buttons }
+      }
+  })
+</script>
+
+<style scoped lang="scss">
+  .wrapper {
+    display: flex;
+    gap: 20px;
+  }
+</style>
