c62a7bc163c8699b280b24fce13e7542749009db
Mukhutdinov Iskander <i.muhutdinov@cs.money>
2023-03-15 21:03:13 +0400

    Добавляет переход на defisec

--- src/components/layout/footer/BrandInfo.vue
+++ src/components/layout/footer/BrandInfo.vue
@@ -1,26 +1,30 @@
 <template>
   <div class="wrapper">
-    <router-link to="/">
-      <InlineSvg :src="require('@/assets/icons/tech-rate-logo.svg')" width="83" height="32" fill="white" title="TechRate"/>
-    </router-link>
+    <div class="images">
+      <router-link to="/">
+        <InlineSvg :src="require('@/assets/icons/tech-rate-logo.svg')" width="83" height="32" fill="white" title="TechRate"/>
+      </router-link>
+
+      <a href="https://defisec.info" target="_blank" style="margin-right: -20px">
+        <InlineSvg :src="require('@/assets/icons/ds-amember-badge.svg')" width="110" height="30" fill="white" title="TechRate"/>
+      </a>
+    </div>
 
     <span class="text">@ 2018 by TechRate. All Rights Reserved</span>
   </div>
 </template>
-
-<script>
-  import { defineComponent } from 'vue';
-
-  export default defineComponent({
-
-  })
-</script>
 
 <style lang="scss" scoped>
   .wrapper {
     display: flex;
     flex-direction: column;
     gap: 30px;
+  }
+
+  .images {
+    display: flex;
+    justify-content: space-between;
+    align-items: baseline;
   }
 
   .text {
